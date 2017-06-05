deb52b62349891f05bba9b6817f7b9465cbe3b6d
Jonathan Gillette <jonathan.gillette@gmail.com>
2017-06-04 21:25:41 -0400

    Creating a usable member's homepage.

--- members/index.php
+++ members/index.php
@@ -4,6 +4,10 @@
 	error_reporting(-1);
 
 	session_start();
+	
+	// TODO:
+	// Validate user can be here (should be built in for all "member" pages.
+	// Get first name for "welcome"
 ?>
 
 <!DOCTYPE html>
@@ -36,26 +40,68 @@
 				</div>
 			</div>
 		</div>
-		<div class="row">
+		<div class="row" style="margin-bottom: 20px;">
 			<div class="col-lg-12">
 				<div class="page-header">
-					<h2>Members</h2>
+					<h2>Welcome <?=$_SESSION["email"]?>!</h2>
 				</div>
-				<?
-					echo 'Login successful' . '<br>';
-					echo 'Email: ' . $_SESSION["email"] . '<br>';
-					echo 'Auth Cd GUID: ' . $_SESSION["auth_cd_guid"] . '<br>';	
-						
-					if(isset($_COOKIE["KCB_Cookie"])) {
-						$pieces = explode(",", $_COOKIE["KCB_Cookie"]);
-					    $cookieEmail = $pieces[0];
-					    $cookieAuthCd = $pieces[1];
-					    
-					    echo 'cookie email: ' . $cookieEmail . '<br>';
-					    echo 'cookie auth Cd: ' . $cookieAuthCd . '<br>';
-					}
-				?>
+				Welcome to the KCB Member section! With this site you can update your information, view the other band member's
+				information, and listen to music the band has played.<br>
 			</div>
+		</div>
+		<div class="row">
+		  <div class="col-sm-6 col-md-4">
+		    <div class="thumbnail">
+		      <img src="../images/info.png" alt="...">
+		      <div class="caption">
+		        <h3>My Info</h3>
+		        <p>Update your information stored with the KCB</p>
+		        <p><a href="#" class="btn btn-default" role="button">Update</a></p>
+		      </div>
+		    </div>
+		  </div>
+		  <div class="col-sm-6 col-md-4">
+		    <div class="thumbnail">
+		      <img src="../images/group-of-people.png" alt="Member Image">
+		      <div class="caption">
+		        <h3>Members</h3>
+		        <p>View the other band member's information</p>
+		        <p><a href="#" class="btn btn-default" role="button">View</a></p>
+		      </div>
+		    </div>
+		  </div>
+		  <div class="col-sm-6 col-md-4">
+		    <div class="thumbnail">
+		      <img src="../images/logo_concert.jpg" alt="Music Image" style="max-height: 150px;">
+		      <div class="caption">
+		        <h3>Music</h3>
+		        <p>View and post links to the music the band plays</p>
+		        <p><a href="#" class="btn btn-default" role="button">Listen</a></p>
+		      </div>
+		    </div>
+		  </div>
+		</div>
+<!--
+		        <div class="col-sm-4">
+			        <a href="#" onfocus="">
+		          <img class="img-circle" src="../images/info.png" alt="Info Image" width="140" height="140">
+		          <h2>My Info</h2>
+		          <p>Update your information stored with the KCB</p>
+			        </a>
+		        </div>
+
+		        <div class="col-sm-4">
+		          <img class="img-circle" src="../images/group-of-people.png" alt="Member Image" width="140" height="140">
+		          <h2>Members</h2>
+		          <p>View the other band member's information</p>
+		        </div>
+
+		        <div class="col-sm-4">
+		          <img class="img-circle" src="../images/logo_concert.jpg" alt="Music Image" width="140" height="140">
+		          <h2>Music</h2>
+		          <p>View and post links to the music the band plays.</p>
+		        </div>
+-->
 		</div>
 		<? require '../includes/footer.php'; ?>
 	</div> <!-- /container -->
