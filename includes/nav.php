b9af86301f18654271c96b2b49ca808f9d0948a7
Jonathan Gillette <jonathan.gillette@gmail.com>
2017-06-08 13:44:35 -0400

    Updated navigation

--- includes/nav.php
+++ includes/nav.php
@@ -21,12 +21,18 @@
 		      <li <? if (basename($_SERVER['PHP_SELF']) == "conductors.php") { ?>class="active"<? } ?>><a href="/conductors.php">Conductors</a></li>
 		      <? if(isset($_SESSION["email"])) { ?>
 					<li class="dropdown <? if ($_SERVER['PHP_SELF'] == "/members/index.php") { ?>active<?}?>">
-			          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Members <span class="caret"></span></a>
-			          <ul class="dropdown-menu">
-			            <li><a href="#">Update account</a></li>
-			            <li role="separator" class="divider"></li>
-			            <li><a href="#">Add new users</a></li>
-					  </ul>
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Members <span class="caret"></span></a>
+					<ul class="dropdown-menu">
+						<li><a href="/members/index.php">Member Home</a></li>
+						<li><a href="#">My Info</a></li>
+						<li><a href="#">Members</a></li>
+						<li><a href="#">Music</a></li>
+						<? if (isset($_SESSION["admin_level"])) { ?>
+						<li role="separator" class="divider"></li>
+						<li><a href="#">Add new users</a></li>
+						<? } ?>
+						<li><a href="logoff.php">Logoff</a></li>
+					</ul>
 		      <? }
 			     else { ?>
 				 	<li <? if (basename($_SERVER['PHP_SELF']) == "members.php") { ?>class="active"<? } ?>><a href="/members.php">Members</a></li>
