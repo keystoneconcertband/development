7fdff9e3bc15e9e38f24f9bd97f070055d33e997
Jonathan Gillette <jonathan.gillette@gmail.com>
2017-07-23 13:12:48 -0400

    Cleanup member homepage

--- members/index.php
+++ members/index.php
@@ -1,31 +1,31 @@
-<?
-	include_once('../includes/class/member.class.php'); 
-	new Member(true);
+<?php
+include_once '../includes/class/member.class.php';
+new Member(true);
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
   <head>
-	<? require '../includes/common_meta.php'; ?>
+	<?php require '../includes/common_meta.php'; ?>
     <meta name="description" content="The Keystone Concert Band member area">
 
     <title>Member Area - Keystone Concert Band</title>
 
-	<? require '../includes/common_css.php'; ?>
-	
-	<!--
-		Not sure what this was for? Removed 6/8/17
+	<?php require '../includes/common_css.php'; ?>
+
 	<style type="text/css">
-		.row .col-lg-12 ul li {
-			margin-bottom: 10px;
+		a.kcb-a:hover {
+			text-decoration: none;
 		}
-		</style>
-	-->
+		.caption h3 {
+			margin-top:0px;
+		}
+	</style>
   </head>
 
   <body>
 
-	<? require '../includes/nav.php'; ?>
+	<?php require '../includes/nav.php'; ?>
 	<div class="container">
 		<div class="row">
 			<div class="col-lg-12">
@@ -39,7 +39,7 @@
 		<div class="row" style="margin-bottom: 20px;">
 			<div class="col-lg-12">
 				<div class="page-header">
-					<h2>Welcome <?=$_SESSION["email"]?>!</h2>
+					<h2>Welcome <?php echo $_SESSION["email"]?>!</h2>
 				</div>
 				Welcome to the KCB Member section! With this site you can update your information, view the other band member's
 				information, and listen to music the band has played.<br>
@@ -47,40 +47,40 @@
 		</div>
 		<div class="row">
 		  <div class="col-sm-6 col-md-4">
-		    <div class="thumbnail">
-		      <img src="../images/info.png" alt="...">
+		  <a href="#" class="kcb-a">
+		    <div class="thumbnail" style="background-color: #f5a651;">
 		      <div class="caption">
 		        <h3>My Info</h3>
 		        <p>Update your information stored with the KCB</p>
-		        <p><a href="#" class="btn btn-default" role="button">Update</a></p>
 		      </div>
 		    </div>
+		  </a>
 		  </div>
 		  <div class="col-sm-6 col-md-4">
-		    <div class="thumbnail">
-		      <img src="../images/group-of-people.png" alt="Member Image">
+		  <a href="#" class="kcb-a">
+		    <div class="thumbnail" style="background-color: #2998dc;">
 		      <div class="caption">
 		        <h3>Members</h3>
 		        <p>View the other band member's information</p>
-		        <p><a href="#" class="btn btn-default" role="button">View</a></p>
 		      </div>
 		    </div>
+		  </a>
 		  </div>
 		  <div class="col-sm-6 col-md-4">
-		    <div class="thumbnail">
-		      <img src="../images/logo_concert.jpg" alt="Music Image" style="max-height: 150px;">
+		    <a href="#" class="kcb-a">
+		    <div class="thumbnail" style="background-color: #16812a">
 		      <div class="caption">
 		        <h3>Music</h3>
 		        <p>View and post links to the music the band plays</p>
-		        <p><a href="#" class="btn btn-default" role="button">Listen</a></p>
 		      </div>
 		    </div>
+		    </a>
 		  </div>
 		</div>
 		</div>
-		<? require '../includes/footer.php'; ?>
+		<?php require '../includes/footer.php'; ?>
 	</div> <!-- /container -->
 
-	<? require '../includes/common_js.php'; ?>
+	<?php require '../includes/common_js.php'; ?>
   </body>
 </html>
