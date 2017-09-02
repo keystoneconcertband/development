9ed1c929dec92992786d0d99872b711f5f6198f0
Jonathan Gillette <jonathan.gillette@gmail.com>
2017-09-02 18:56:34 -0400

    moved javascript to its own file

---
+++ kcb-js/myInfo.js
@@ -0,0 +1,83 @@
+$(document).ready(function () {
+	$('#addRow').click(function () {
+		$('<div/>', {
+			'class' : 'col-lg-12 extraEmail', html: GetHtml()
+		}).hide().appendTo('#emailContainer').slideDown('slow'); //Get the html from template and hide and slideDown for transtion.
+	});
+});
+
+$("#memberInfo").validator({
+    custom: {
+        'carrier': function($el) {
+	        var cell = $("#inputCellPhoneNbr").val();
+	        var carrier = $("#inputCarrier").val();
+	        
+	        if(cell !== '' && carrier === '0') {
+		    	return "Carrier is required when cell phone nbr is entered.";   
+	        }
+	    }
+    }
+});
+
+$("#memberInfo").validator().on("submit", function (event) {
+    if (event.isDefaultPrevented()) {
+        formError();
+        submitMSG(false, "Oops! Looks like you have a validation error. Check for errors in the form.");
+    } else {
+        event.preventDefault();
+        submitForm();
+    }
+});
+
+function submitForm() {
+	$.ajax(
+	{
+        url: "",
+        type: "POST",
+		dataType : 'json', 
+        data: $("#memberInfo").serialize(),
+        success: function(text){
+            if (text == "success"){
+                formSuccess();
+            } else {
+                formError();
+                submitMSG(false,text);
+            }
+        },
+		error: function(xhr, resp, text) {
+            console.log(xhr, resp, text);
+        }
+    });
+}
+
+function GetHtml() //Get the template and update the input field names
+{
+	var len = $('.extraEmail').length + 1;
+	var $html = $('.extraEmailTemplate').clone();
+	$html.find('[name=lblEmail]')[0].id="lblEmail" + len;
+	$html.find('[name=lblEmail]').text("Email " + len);
+	$html.find('[name=lblEmail]').attr('for', "inputEmail" + len);
+	$html.find('[name=lblEmail]').attr('name', "lblEmail" + len);
+	$html.find('[name=inputEmail]')[0].name="inputEmail" + len;
+	
+	return $html.html();    
+}
+
+function formSuccess(){
+    submitMSG(true, "Your information has been updated!")
+}
+
+function formError(){
+    $("#contactForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+        $(this).removeClass();
+    });
+}
+
+function submitMSG(valid, msg){
+    if(valid){
+        var msgClasses = "h4 tada animated text-success";
+    } else {
+        var msgClasses = "h4 text-danger";
+    }
+    $("#msgSubmit").removeClass().addClass(msgClasses).text(msg);
+}
