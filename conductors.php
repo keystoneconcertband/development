80dfc67e017568436cf841b598dd5f42ed282fc6
Jonathan Gillette <jonathan.gillette@gmail.com>
2018-03-09 17:44:07 -0500

    Updated conductors text

--- conductors.php
+++ conductors.php
@@ -39,37 +39,37 @@
 					<img alt="" src="images/donnaDeaven.jpg" style="float: left; margin-right: 10px;"> 
 					Donna J. Deaven has more than 12 years of experience in conducting bands, 
 					orchestras, and small ensembles, and 15 years of experience as a private 
-					teacher and professional musician.<br><br>Ms. Deaven graduated with 
-					a Bachelor of Science degree in Music Education and a Master of Music 
-					degree in saxophone performance from West Chester University, where 
-					she studied with Ted Hegvik.<br><br>Ms. Deaven is also the Assistant 
-					Conductor of the New Cumberland Town Band, a founding member and baritone 
-					saxophone player in the Keystone Saxophone Quartet, a member of the 
-					Flutes of Fancy Flute Ensemble, and a performing member of the Wednesday 
+					teacher and professional musician.<br><br>
+					Ms. Deaven graduated with a Bachelor of Science degree in Music Education 
+					and a Master of Music degree in saxophone performance from West Chester University, 
+					where she studied with Ted Hegvik.<br><br>
+					Ms. Deaven is also the Assistant Conductor of the New Cumberland Town Band, 
+					a founding member and baritone saxophone player in the Keystone Saxophone Quartet, 
+					a member of the Flutes of Fancy Flute Ensemble, and a performing member of the Wednesday 
 					Club. She is also Music Director/Conductor of the Harrisburg Chamber 
 					Ensemble. In addition, Ms. Deaven leads a combo called "Donna and the 
-					Daytimers."<br><br>Ms.Deaven's previous affiliations include the New 
+					Daytimers."<br><br>
+					Ms.Deaven's previous affiliations include the New 
 					Singer Band of Mechanicsburg, the West Shore Symphony Orchestra, the 
 					Hershey Symphony Orchestra, Harrisburg Area Community College, the Harrisburg 
-					Community Theater, and York Little Theater.<br><br>In addition to conducting, 
-					Ms. Deaven's education and experience include studying and performing 
-					on the saxophone, flute, clarinet and percussion. She also maintains 
+					Community Theater, and York Little Theater.<br><br>
+					In addition to conducting, Ms. Deaven's education and experience include studying and 
+					performing on the saxophone, flute, clarinet and percussion. She also maintains 
 					a private woodwind teaching studio in Camp Hill. <br><br>
 					<h3>John Hope, Assistant Conductor</h3>
 					<img alt="" src="images/johnHope.png" style="float: left; margin-right: 10px;"> 
 					John Hope has always regretted not learning to play the piano when his 
 					mother was teaching other children in their Philadelphia home. He did, 
 					however, take drum lessons in elementary school and played in the elementary 
-					and junior high school orchestras.<br><br>Later, he played in the Ursinus 
-					College band for two years. More of his time was spent, however, as 
-					a member of the Ursinus College touring choir, for which John was the 
-					student conductor in his last three years in school.<br><br>He joined 
-					the Keystone Concert Band percussion section more than 10 years ago. 
-					A few years later, conductor Donna Deaven asked him to lead a march 
-					at rehearsal while she dealt with another issue. One thing led to another 
-					and he is now officially the band's assistant conductor, conducting 
-					one or two numbers at each concert and on notice to fill in for Donna 
-					if she is unable to be at a rehearsal or concert.
+					and junior high school orchestras.<br><br>Later, he played in the Ursinus College
+					band for two years. More of his time was spent, however, as a member of the Meistersingers,
+					the college touring concert choir, for which John was the student conductor in his last
+					three years in school.<br><br>
+					He joined the Keystone Concert Band percussion section more than 15 years ago. 
+					A few years later, conductor Donna Deaven asked him to lead a march at rehearsal while 
+					she dealt with another issue. He is now the band's assistant conductor, 
+					leading one or two numbers at each concert and taking over for Donna 
+					when she is unable to be at a rehearsal or concert.
 				</div>
 			</div>
 		</div>
