566c000171bdf2d5580e2de4ca70e055dc9304b4
Jonathan Gillette <jonathan.gillette@gmail.com>
2018-03-15 18:25:19 -0400

    Added google analytics

--- includes/common_js.php
+++ includes/common_js.php
@@ -5,3 +5,11 @@
     <script src="/bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <script src="/ie10-viewport-bug/workaround.js"></script>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-70031-3"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+	  gtag('config', 'UA-70031-3');
+	</script>
