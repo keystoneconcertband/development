d560e562c874a1bc2132f9710467eec324b9dde8
Jonathan <jonathan.gillette@gmail.com>
2021-06-10 21:35:43 -0400

    Added v4 and 5 of bootswatch; v5 of bootstrap and latest jQuery

--- includes/common_js.php
+++ includes/common_js.php
@@ -1,8 +1,8 @@
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="/3rd-party/jquery/jquery-3.5.1.min.js"></script>
-    <script src="/3rd-party/bootstrap-3.4.1-dist/js/bootstrap.min.js"></script>
+    <script src="/3rd-party/jquery/jquery-3.6.0.min.js"></script>
+    <script src="/3rd-party/bootstrap-4.6.0-dist/js/bootstrap.min.js"></script>
     <!-- Global site tag (gtag.js) - Google Analytics -->
 	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-70031-3"></script>
 	<script>
