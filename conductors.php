465302b499fa0a38b8682132d10ac98e2d4c0d98
Jonathan <jonathan.gillette@gmail.com>
2021-11-07 16:41:41 -0500

    Removed John Hope

--- conductors.php
+++ conductors.php
@@ -37,19 +37,16 @@
 				<div class="container-fluid">
 					<div class="row">
 						<div class="col-md-4">
-							<img alt="KCB Conductors - Chris Mack, Donna Deaven, John Hope" src="images/conductors-1x.png" srcset="images/conductors-2020.png" class="img-responsive"> 
-							<p><small>From left to right: Chris Mack, Donna Deaven, John Hope</small></p>
+							<img alt="KCB Conductors - Chris Mack, Donna Deaven" src="images/conductors-1x.png" srcset="images/conductors-2021.png" class="img-responsive"> 
+							<p><small>From left to right: Chris Mack, Donna Deaven</small></p>
 						</div>
 						<div class="col-md-8">
 							<ul class="nav nav-pills">
-							  <li>
-							    <a data-toggle="tab" href="#chris">Chris Mack</a>
-							  </li>
 							  <li class="active">
 							    <a data-toggle="tab" href="#donna">Donna Deaven</a>
 							  </li>
 							  <li>
-							    <a data-toggle="tab" href="#john">John Hope</a>
+							    <a data-toggle="tab" href="#chris">Chris Mack</a>
 							  </li>
 							</ul>
 							<div id="conductorsTabContent" class="tab-content">
@@ -95,21 +92,6 @@
 								at Messiah College in the summer of 2019.
 								<br><br>
 								Chris continues to lead the Keystone Concert Band as the third conductor alongside Donna and John.
-							  </div>
-							  <div class="tab-pane fade" id="john">
-							    <h3>John Hope, Assistant Conductor</h3>
-								John Hope has always regretted not learning to play the piano when his 
-								mother was teaching other children in their Philadelphia home. He did, 
-								however, take drum lessons in elementary school and played in the elementary 
-								and junior high school orchestras.<br><br>Later, he played in the Ursinus College
-								band for two years. More of his time was spent, however, as a member of the Meistersingers,
-								the college touring concert choir, for which John was the student conductor in his last
-								three years in school.<br><br>
-								He joined the Keystone Concert Band percussion section more than 15 years ago. 
-								A few years later, conductor Donna Deaven asked him to lead a march at rehearsal while 
-								she dealt with another issue. He is now the band's assistant conductor, 
-								leading one or two numbers at each concert and taking over for Donna 
-								when she is unable to be at a rehearsal or concert.
 							  </div>
 							</div>
 						</div>
