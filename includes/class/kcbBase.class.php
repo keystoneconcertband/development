621556a81791a640bced08d319c973c4f682f3c0
Jonathan <jonathan.gillette@gmail.com>
2023-11-11 18:02:00 -0500

    added return

--- includes/class/kcbBase.class.php
+++ includes/class/kcbBase.class.php
@@ -44,11 +44,16 @@ class KcbBase
         $sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
         try {
             $response = $sendgrid->send($email);
-            print $response->statusCode() . "\n";
-            print_r($response->headers());
-            print $response->body() . "\n";
+
+            if($response->statusCode() < 400) {
+                return true;
+            }
+            else {
+                return false;
+            }
         } catch (Exception $e) {
             echo 'Caught exception: '. $e->getMessage() ."\n";
+            return false;
         }
     }
 
