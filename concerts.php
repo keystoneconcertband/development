2c6107ab9ce2904b29be56f5eb35e306f0eda808
Jonathan <jonathan.gillette@gmail.com>
2025-08-01 21:42:13 -0400

    Fixing accordion

--- concerts.php
+++ concerts.php
@@ -37,9 +37,6 @@
 				<div class="page-header">
 					<h2>Concert Schedule</h2>
 				</div>
-<!--
-				<div class='alert alert-info'><strong>More concerts still yet to come!</strong><br />Continue to check back again throughout the season to see any additional concerts added to the lineup!</div>
--->
 				<?php
 					$rowNbr = 1;
 					$concerts = $cncrts->getConcertSchedule();
@@ -48,6 +45,7 @@
 						echo "<div class='alert alert-info'><strong>Looks like we don't have any concerts posted yet.</strong><br />Check back again later in the Spring!</div>";
 					}
 					else {
+						echo "<div class='accordion' id='accordion'>\n";
 						foreach ($concerts as $concert) {		
 							$disabled = '';
 							$today = date("Y-m-d");
@@ -64,47 +62,47 @@
 								$disabled = 'text-muted';
 							}
 							
-							echo "<div class='panel-group' id='accordion'>\n";
-							echo "<div class='panel panel-default' id='panel" . $rowNbr . "'>\n";
-							echo "  <div class='panel-heading " . $disabled . "'>\n";
-							echo "	  <div class='panel-title'>";
-							echo "      <a data-bs-toggle='collapse' data-bs-target='#collapse" . $rowNbr . "' href='#collapse'>\n";
-							echo 			$concert['Title'];
-							echo "      </a>\n";
-							echo "    </div>\n";
-							echo "  </div>\n";
-							echo "  <div id='collapse" . $rowNbr . "' class='panel-collapse collapse ";
-							
-							if (!$disabled) { 
-								echo "in"; // starts the panel in an open state
+							echo "  <div class='accordion-item'>\n";
+							echo "    <h2 class='accordion-header'>\n";
+							if($disabled) {
+								echo "  <button class='accordion-button collapsed' type='button' data-bs-toggle='collapse' data-bs-target='#collapse" . $rowNbr . "' aria-expanded='false' aria-controls='collapse" . $rowNbr . "'>" . $concert['Title'] . "</button>\n";
 							}
-							
-							echo "'>\n";
-							echo "    <div class='panel-body'>\n";
-							echo "		<p class='" . $disabled . "'>\n";
-							echo "      <h3 style='margin-top:0px;'>" . $concert['Title'] . "</h3><h4> " . $begin . " at " . date('g:iA', strtotime($concert['concertBegin'])) . ".</h4>\n";
-							echo "        <a class='" . $disabled . "' href='https://maps.google.com/maps?q=" . urlencode($concert['address']) . "' target='_blank' style='border-bottom:none;'>" . $concert['address'] . "</a>\n";
-							echo "      </p>\n";
-							echo "		<div style='width: 100%'><iframe width='100%' height='340' frameborder='0' scrolling='no' marginheight='0' marginwidth='0' src='https://maps.google.com/maps?width=100%25&amp;height=340&amp;hl=en&amp;q=" . urlencode($concert['address']) ."&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed'></iframe></div>";
-							echo "      <div class='band-member-notice well well-sm' style='max-width:340px;'>";
-							echo "		  <h4>Band Members: </h4>";
-							echo "			<ul class='list-group'>";
+							else {
+								echo "  <button class='accordion-button' type='button' data-bs-toggle='collapse' data-bs-target='#collapse" . $rowNbr . "' aria-expanded='true' aria-controls='collapse" . $rowNbr . "'>" . $concert['Title'] . "</button>\n";
+							}
+							echo "    </h2>\n";
+							echo "    <div id='collapse" . $rowNbr . "' class='accordion-collapse collapse" . ($disabled ? '' : ' show') . "'>";
+							echo "      <div class='accordion-body'>";
+							echo "        <div class='panel-body'>\n";
+							echo "		    <p class='" . $disabled . "'>\n";
+							echo "            <h3 style='margin-top:0px;'>" . $concert['Title'] . "</h3><h4> " . $begin . " at " . date('g:iA', strtotime($concert['concertBegin'])) . ".</h4>\n";
+							echo "              <a class='" . $disabled . "' href='https://maps.google.com/maps?q=" . urlencode($concert['address']) . "' target='_blank' style='border-bottom:none;'>" . $concert['address'] . "</a>\n";
+							echo "          </p>\n";
+							echo "		    <div style='width: 100%'><iframe width='100%' height='340' frameborder='0' scrolling='no' marginheight='0' marginwidth='0' src='https://maps.google.com/maps?width=100%25&amp;height=340&amp;hl=en&amp;q=" . urlencode($concert['address']) ."&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed'></iframe></div>";
+							echo "          <div class='card' style='max-width:340px;'>";
+							echo "            <div class='card-body'>";
+							echo "		        <h5 class='card-title'>Band Members</h5>";
+							echo "			    <ul class='list-group list-group-flush'>";
 			  				if ($concert['pants'] == 0) {
-								echo "		  <li class='list-group-item'><span class='glyphicon glyphicon-alert' aria-hidden='true'></span> This is a black pants concert</li>\n";
+								echo "		      <li class='list-group-item'><span class='glyphicon glyphicon-alert' aria-hidden='true'></span> This is a black pants concert</li>\n";
 							}
 							elseif ($concert['pants'] == 1) {
-								echo "		  <li class='list-group-item'><span class='glyphicon glyphicon-alert' aria-hidden='true'></span> This is a tan pants concert</li>\n";
+								echo "		        <li class='list-group-item'><span class='glyphicon glyphicon-alert' aria-hidden='true'></span> This is a tan pants concert</li>\n";
 							}
 							if ($concert['chair'] == 1) {
-								echo "		  <li class='list-group-item'><span class='glyphicon glyphicon-alert' aria-hidden='true'></span> A chair is required for this concert</li>\n";
+								echo "		        <li class='list-group-item'><span class='glyphicon glyphicon-alert' aria-hidden='true'></span> A chair is required for this concert</li>\n";
 							}
-							echo "		</div>";
-							echo "    </div>\n";								
-							echo "  </div>\n";
-							echo "</div>\n";
+							echo "			    </ul>";
+							echo "		      </div>"; // end card-body
+							echo "		    </div>"; // end band-member-notice
+							echo "        </div>\n"; // end panel-body							
+							echo "      </div>\n"; // end accordion-body
+							echo "    </div>\n"; // end collapse
+							echo "  </div>\n"; // end accordion-item
 							
 							$rowNbr++;
 						}
+						echo "</div>\n"; // end panel-group
 					}
 				?>
 			</div>
