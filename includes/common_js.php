22dd37b2cc913263c64a73335f7f0c107466b5da
Jonathan <jonathan.gillette@gmail.com>
2025-08-07 21:18:38 -0400

    Changes to support join page (still in progress)

--- includes/common_js.php
+++ includes/common_js.php
@@ -2,6 +2,7 @@
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="/3rd-party/bootstrap-5.3.7-dist/js/bootstrap.min.js"></script>
+    <script src="/3rd-party/jquery/jquery-3.7.1.min.js"></script>
 	<!-- Google tag (gtag.js) -->
 	<script async src="https://www.googletagmanager.com/gtag/js?id=G-EBPFTM35MW"></script>
 	<script>
