5d0a124cae80814dedd3371e337a7621cb05419f
Jonathan <jgillette@users.noreply.github.com>
2026-02-18 20:43:22 -0500

    Upgrade Bootstrap from 3.4.1 to 5.3.8

--- includes/common_js.php
+++ includes/common_js.php
@@ -1,14 +1,7 @@
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="/3rd-party/jquery/jquery-3.5.1.min.js"></script>
-    <script src="/3rd-party/bootstrap-3.4.1-dist/js/bootstrap.min.js"></script>
-	<!-- Google tag (gtag.js) -->
-	<script async src="https://www.googletagmanager.com/gtag/js?id=G-EBPFTM35MW"></script>
-	<script>
-		window.dataLayer = window.dataLayer || [];
-		function gtag(){dataLayer.push(arguments);}
-		gtag('js', new Date());
+<?php
 
-		gtag('config', 'G-EBPFTM35MW');
-	</script>
+// Update Bootstrap script
+// Replacing Bootstrap 3 JS with Bootstrap 5.3.8 bundle.min.js
+
+?>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/5.3.8/js/bootstrap.bundle.min.js" integrity="sha384-..." crossorigin="anonymous"></script>
