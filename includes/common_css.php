8d626e86516314606af1789d44880b8eb1b20395
Jonathan <jonathan.gillette@gmail.com>
2026-05-08 21:19:42 -0400

    Fixed the css reference

--- includes/common_css.php
+++ includes/common_css.php
@@ -1,4 +1,5 @@
+    <?php require_once __DIR__ . '/asset.php'; ?>
     <!-- YETI version of bootstrap core CSS -->
     <link href="/3rd-party/bootswatch-5.3.7/yeti/bootstrap.min.css" rel="stylesheet">
-    <link href="/css/site-20250801.css" rel="stylesheet">
+    <link href="<?= asset('/css/site.css') ?>" rel="stylesheet">
     <link href="https://netdna.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
