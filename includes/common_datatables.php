7f96bf67718d6bea145087cff3404a943bf83b57
Jonathan <jonathan.gillette@gmail.com>
2026-05-10 13:43:28 -0400

    Adding bootstap 5 styling to data tables. Removed old 3rd-party items

--- includes/common_datatables.php
+++ includes/common_datatables.php
@@ -1,3 +1,5 @@
 	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/2.3.8/css/dataTables.dataTables.min.css"/>
-	<script type="text/javascript" src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/2.3.8/css/dataTables.bootstrap5.css"/>
+	<script type="text/javascript" src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 	<script type="text/javascript" src="https://cdn.datatables.net/2.3.8/js/dataTables.min.js"></script>
+	<script type="text/javascript" src="https://cdn.datatables.net/2.3.8/js/dataTables.bootstrap5.js"></script>
