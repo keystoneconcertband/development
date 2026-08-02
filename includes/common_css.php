49c89a1cef36c2a52edb7336f4d7d0d9f47d17d0
Jonathan <jgillette@users.noreply.github.com>
2026-08-02 19:16:56 -0400

    Migrate dependencies to Composer and update member navigation (#139)

--- includes/common_css.php
+++ includes/common_css.php
@@ -1,4 +1,4 @@
     <?php require_once __DIR__ . '/asset.php'; ?>
-    <link href="/3rd-party/bootswatch-5.3.7/yeti/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@5.3.8/dist/yeti/bootstrap.min.css" integrity="sha384-OMwG/TAHy7NRQbZ6SZ/45S4g8n76iLIAkbYP8evydAdSZiO97yIyh5g2zThHlY8r" crossorigin="anonymous">
     <link href="<?= asset('/css/site.css') ?>" rel="stylesheet">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet" integrity="sha384-iw3OoTErCYJJB9mCa8LNS2hbsQ7M3C0EpIsO/H5+EGAkPGc6rk+V8i04oW/K5xq0" crossorigin="anonymous">
--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
       <hr class="featurette-divider">
       <footer>
         <p class="float-end"><a href="#">Back to top</a></p>
-        <p>&copy; <?php echo date("Y"); ?> Keystone Concert Band | Site developer <a href="http://www.jonathangillette.net">Jonathan Gillette</a></p>
+        <p>&copy; <?php echo date("Y"); ?> Keystone Concert Band | Site developer <a href="https://www.jonathangillette.net">Jonathan Gillette</a></p>
       </footer>
